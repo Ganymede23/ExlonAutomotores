cbe2985537e6ef351340fa51d5424a906919ea75
kicillof <cuentasdebusso@hotmail.com>
2019-11-06 04:31:30 -0300

    Funcionando. Backup realizado. Vista y procedimiento almacenado funcionando en el form. Queda revisar indices.

--- ExlonApp/Form1.Designer.cs
+++ ExlonApp/Form1.Designer.cs
@@ -28,13 +28,359 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkbox1 = new System.Windows.Forms.CheckedListBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(830, 671);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(454, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(790, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(522, 82);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ALTA OPERACIONES";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(1076, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 38);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(790, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(522, 681);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(824, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ID Vehiculo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(824, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fecha";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(824, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 31);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tipo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(824, 404);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 31);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "- Tipo Doc.";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(1076, 401);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(208, 38);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(824, 354);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(164, 31);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "EMPLEADO";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(824, 304);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 31);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Monto";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(1076, 301);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(208, 38);
+            this.textBox6.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(824, 554);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(147, 31);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "- Tipo Doc.";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(1076, 551);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(208, 38);
+            this.textBox7.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(824, 504);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(130, 31);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "CLIENTE";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(824, 454);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(146, 31);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "- Nro. Doc.";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(1076, 451);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(208, 38);
+            this.textBox9.TabIndex = 15;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(912, 168);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(144, 24);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "(AAAA-MM-DD)";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(897, 214);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(143, 24);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "(Compra/Venta)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(1076, 161);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 38);
+            this.textBox2.TabIndex = 5;
+            // 
+            // checkbox1
+            // 
+            this.checkbox1.CheckOnClick = true;
+            this.checkbox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkbox1.FormattingEnabled = true;
+            this.checkbox1.Items.AddRange(new object[] {
+            "Compra",
+            "Venta"});
+            this.checkbox1.Location = new System.Drawing.Point(1076, 214);
+            this.checkbox1.Name = "checkbox1";
+            this.checkbox1.Size = new System.Drawing.Size(143, 74);
+            this.checkbox1.TabIndex = 23;
+            this.checkbox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.CheckedListBox1_ItemCheck);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(824, 604);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(146, 31);
+            this.label14.TabIndex = 25;
+            this.label14.Text = "- Nro. Doc.";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(1076, 601);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(208, 38);
+            this.textBox3.TabIndex = 24;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(163, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(454, 68);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "Ventas del ultimo año";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label15
+            // 
+            this.label15.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(125, 2);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(522, 82);
+            this.label15.TabIndex = 27;
+            this.label15.Text = "CONSULTA";
+            this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 189);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(722, 550);
+            this.dataGridView1.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1334, 787);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkbox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckedListBox checkbox1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- ExlonApp/Form1.cs
+++ ExlonApp/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace ExlonApp
 {
@@ -15,6 +17,98 @@ namespace ExlonApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"DESKTOP-BL144A7";
+            builder.InitialCatalog = "Exlon";
+            builder.IntegratedSecurity = true;
+
+            var conditionstring = builder.ToString();
+
+            //var query = @"SELECT * FROM ListadoOperacionesUltimoAnio";
+
+            //var query2 = @"EXECUTE AltaOperaciones 'VALOR-PARAMETRO1','VALOR-PARAMETRO2';";
+
+            //var query = @"select*
+            //                from Personas
+            //                where nro_doc = '37421451'";
+
+            //using (SqlConnection sql = new SqlConnection(conditionstring))
+            //{
+            //    using (SqlCommand cmd = new SqlCommand(query, sql))
+            //    {
+            //        DataTable dt = new DataTable(); //Tabla de donde se levantan los datos.
+            //        SqlDataAdapter da = new SqlDataAdapter(cmd); //Lee el query. Se usa cuando lees datos.
+
+            //        sql.Open();
+            //        da.Fill(dt); //da recibe los datos del dt.
+
+            //        int c = 0;
+            //        c++;
+            //    }
+            //}
+
+            using (SqlConnection sql = new SqlConnection(conditionstring))
+            {
+                using (SqlCommand cmd = new SqlCommand("AltaOperaciones", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@id_vehiculo", textBox1.Text.ToString()));
+                    cmd.Parameters.Add(new SqlParameter("@fecha_op", textBox2.Text.ToString()));
+                    cmd.Parameters.Add(new SqlParameter("@tipo_operacion", checkbox1.SelectedItem.ToString()));
+                    cmd.Parameters.Add(new SqlParameter("@monto_total", textBox6.Text.ToString()));
+                    cmd.Parameters.Add(new SqlParameter("@tipo_doc_empleado", textBox4.Text.ToString()));
+                    cmd.Parameters.Add(new SqlParameter("@nro_doc_empleado", textBox9.Text.ToString()));
+                    cmd.Parameters.Add(new SqlParameter("@tipo_doc_cliente", textBox7.Text.ToString()));
+                    cmd.Parameters.Add(new SqlParameter("@nro_doc_cliente", textBox3.Text.ToString()));
+
+                    sql.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void CheckedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.NewValue == CheckState.Checked && checkbox1.CheckedItems.Count > 0)
+            {
+                checkbox1.ItemCheck -= CheckedListBox1_ItemCheck;
+                checkbox1.SetItemChecked(checkbox1.CheckedIndices[0], false);
+                checkbox1.ItemCheck += CheckedListBox1_ItemCheck;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"DESKTOP-BL144A7";
+            builder.InitialCatalog = "Exlon";
+            builder.IntegratedSecurity = true;
+
+            var conditionstring = builder.ToString();
+
+            var query = @"SELECT * FROM ListadoOperacionesUltimoAnio";
+
+            //var query = @"select*
+            //                from Personas
+            //                where nro_doc = '37421451'";
+
+            using (SqlConnection sql = new SqlConnection(conditionstring))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sql))
+                {
+                    DataTable dt = new DataTable(); //Tabla de donde se levantan los datos.
+                    SqlDataAdapter da = new SqlDataAdapter(cmd); //Lee el query. Se usa cuando lees datos.
+
+                    sql.Open();
+                    da.Fill(dt); //da recibe los datos del dt.
+
+                    dataGridView1.DataSource = dt;
+                }
+            }
         }
     }
 }
